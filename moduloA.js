dd9f508a484ac3d480812de83bae012285d436f3
LeonardodeAraujoAlves <78648608+LeonardodeAraujoAlves@users.noreply.github.com>
2022-06-11 13:05:26 -0300

    teste com node

---
+++ moduloA.js
@@ -0,0 +1,3 @@
+this.ola = 'Fala Pessoal';
+exports.bemVindo = 'bem vindo ao node';//froma de exportar algo de algum modulo
+module.exports.atelogo = 'Até o próximo exemplo';
---
+++ moduloCliente.js
@@ -0,0 +1,7 @@
+//Acessando os modulos A e B
+
+const moduloA = require('./moduloA');
+const moduloB = require('./moduloB');
+
+console.log(moduloA.atelogo);
+console.log(moduloA);//mostra os atributos do modulo A
